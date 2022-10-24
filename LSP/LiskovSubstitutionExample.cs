8302aa074fa7eeea1a931c9c5870c7eeeaf423a4
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-24 18:42:17 +0200

    Add an specific example of liskov substitution principle

---
+++ LSP/LiskovSubstitutionExample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LSP
+{
+    // Liskov substitution principle - (zasada podstawienia Liskov) - metody które
+    // używają wskaźników lub refenrencji do klas bazowych muszą być w stanie używać również obiektów
+    // klas dziedziczących po klasach bazowych bez znajomości tych obiektów.
+    
+    // Najbardziej popularny przykład
+    public abstract class Duck
+    {
+        public abstract void Swim();
+        public abstract void Quack();
+    }
+
+    public class RegularDuck : Duck
+    {
+        public override void Swim()
+        {
+            Console.WriteLine("regular duck swim");
+        }
+
+        public override void Quack()
+        {
+            Console.WriteLine("regular duck quack");
+        }
+    }
+
+    public class ElectricDuck : Duck
+    {
+        public bool IsTurnOn = false;
+
+        public void TurnOnDuck()
+        {
+            IsTurnOn = true;
+        }
+
+        public void TurnOffDuck()
+        {
+            IsTurnOn = false;
+        }
+        
+        public override void Swim()
+        {
+            if (!IsTurnOn)
+                return;
+            Console.WriteLine("electric duck swim");
+        }
+
+        public override void Quack()
+        {
+            Console.WriteLine("electric duck quack");
+        }
+    }
+}
---
+++ OC/OpenCloseExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OC
+{
+    // Open Close principle - klasa powinna być zamknięta na modyfikacje i otwarta na rozszerzenia
+    public class DataPersistence
+    {
+        private Data data;
+        private IDataSaver _dataSaver;
+
+        public DataPersistence(Data data, IDataSaver dataSaver)
+        {
+            this.data = data;
+            _dataSaver = dataSaver;
+        }
+
+        // dzięki tej metodzie nie ma potrzeby modyfikować klasy ponieważ jak chcemy zmodyfikować Save wstrzykujemy inną klasę implementującą intrfejs IDataSaver
+        public void Save()
+        {
+            _dataSaver.Save(data); // wydzielająć implemenetacje do pewnej abstrackcji otwieramy klasę na rozszerzenia
+        }
+    }
+
+    public class Data
+    {
+        public string SomeData { get; set; }
+    }
+
+    public interface IDataSaver
+    {
+        void Save(Data data);
+    }
+
+    public class ExcelSaver : IDataSaver
+    {
+        public void Save(Data data)
+        {
+            Console.WriteLine($"saving data: {data.SomeData} as excel file");
+        }
+    }
+}
