e7fdd54b44a855979e9d58546009d611cbe86cc3
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-24 15:49:59 +0200

    Add an example of open close principle

---
+++ OC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new() { SomeData = "data #1" };
+
+            DataPersistence persistence = new DataPersistence(data, new ExcelSaver());
+            persistence.Save();
+        }
+    }
+}
