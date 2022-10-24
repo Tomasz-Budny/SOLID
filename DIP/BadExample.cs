c4afef22005f9db3c760af600492bb04834298c2
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-24 19:42:50 +0200

    Add an example of dependency inversion principle

---
+++ DIP/BadExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DIP
+{
+   public class EmployeeRepositoryBad
+   {
+      public void Add(Employee employee)
+      {
+         Console.WriteLine("Add employee to database");
+      }
+   }
+
+   // Moduł wysokopoziomowy - ponieważ używa EmployeeRepository
+   //   KLASA EMPLOYEE SERVICE ZALEŻY OD EMPLOYEE RESPOSITORY WIĘC JEST TO ZŁAMANIE ZASADY DIP 
+   // moduł wysokopoziomowy nie może zależeć od moddułu niskopoziomowego
+   public class EmployeeServiceBad
+   {
+      // Moduł niskopoziomowy - ponieważ jest w EmployeeService
+      private readonly EmployeeRepositoryBad _employeeRepository = new EmployeeRepositoryBad();
+
+      public void Add(Employee employee)
+      {
+         _employeeRepository.Add(employee);
+      }
+   }
+}
---
+++ DIP/DependencyInversionExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DIP
+{
+    // Dependency inversion principle - zasada odwrócenia zależności 
+    // moduły wysokopoziomowe nie powinny zależeć od modułów niskopoziomowych, oba powinny zależeć od abstrakcji
+    
+    // w skrócie - abstrakcje nie powinny zależeć od szczegółów. To szczegóły powinny zależeć od abstrakcji.
+    
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+
+    public interface IEmployeeRepository
+    {
+        void Add(Employee employee);
+    }
+
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void Add(Employee employee)
+        {
+            Console.WriteLine("Add employee to database");
+        }
+    }
+
+    // Teraz moduł wysokopizomowy nie jest zależny od niskopoziomowgo lecz od abstrakcji
+    // wieć zasada nie jest naruszona
+    public class EmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public void Add(Employee employee)
+        {
+            _employeeRepository.Add(employee);
+        }
+    }
+
+}
