4bb200207b8b7a8c4756baa85c706ad5c0e3a1ce
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-24 18:41:31 +0200

    Add an example of Liskov substitution principle

---
+++ LSP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ElectricDuck electric = new ElectricDuck();
+            RegularDuck regular = new RegularDuck();
+            
+            List<Duck> ducks = new List<Duck>();
+            ducks.Add(electric);
+            ducks.Add(regular);
+
+            // elektryczna kaczka nie popłynie ponieważ nie jest włączona 
+            // abstrakcja taka narusza zasadę podstawienia Liskov, należy ją zmienić
+            foreach (var duck in ducks)
+            {
+                duck.Swim();
+            }
+        }
+    }
+}
